fcb88c9d6fbf800f620621a82d7031d5eeb6f8aa
Azixe <thedragonswordminecraft@gmail.com>
2024-10-28 22:57:30 +0700

    Nyicil navbar dan sidebar code

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,5 @@
-import orderModel from "../models/orderModel";
-import userModel from "../models/userModel";
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
 //menempatkan order untuk frontend
@@ -14,10 +14,10 @@ export const placeOrder = async (req, res) => {
         await newOrder.save();
         await userModel.findByIdAndUpdate(req.body.userID,{cartData:{}})
         //masih ada method yang belom di add seperti payment, belom setup api nya
-
+ 
     } catch (error) {
         
     }
 }
 
-export {placeOrder}
+// export {placeOrder}
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,7 +1,9 @@
 import express from "express"
 //unfinished. need authentication
-import { placeOrder } from "../controllers/orderController"
+import { placeOrder } from "../controllers/orderController.js"
 
 const orderRouter = express.Router()
 
 orderRouter.post("/place",placeOrder);//authentication lom di implement
+
+export default orderRouter;
