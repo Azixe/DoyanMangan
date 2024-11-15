998a9a1427b5adb7ce01c817abd5255139d20794
Azixe <thedragonswordminecraft@gmail.com>
2024-11-15 23:00:04 +0700

    Melengkapi Add function di admin (msh incomplete)

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Add.css'
+
 const Add = () => {
   return (
     <div className='add'>
@@ -23,17 +24,22 @@ const Add = () => {
           <div className="add-category flex-col">
             <p>Product category</p>
             <select name="category">
-              <option value=""></option> 
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
-              <option value=""></option>
+              <option value="Salad">Salad</option> 
+              <option value="Rolls">Rolls</option>
+              <option value="Deserts">Deserts</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure Veg">Pure Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
             </select>
           </div>
+          <div className='add-price flex-col'>
+            <p>Product Price</p>
+            <input type="Number" name="price" placeholder='$20' />
+          </div>
         </div>
+        <button type='submit' className='add-btn'>ADD</button>
       </form>
     </div>
   )
