480f1ece1ae968cc44e99f87fc6d8554d8aa714d
Azixe <thedragonswordminecraft@gmail.com>
2024-11-16 13:18:43 +0700

    Added shopping cart functionality

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
+import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
 import 'dotenv/config'
 
@@ -23,7 +24,7 @@ connectDB();
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
-// app.use("/api/cart",cartRouter)
+app.use("/api/cart",cartRouter)
 app.use("/api/order",orderRouter)
 
 app.get("/", (req,res)=>{
